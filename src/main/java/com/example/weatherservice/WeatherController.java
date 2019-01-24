eecbd6cb7d1e7b793ca6e70abe329547d204f985
otty375 <sie.jugend.15@gmail.com>
2019-01-24 21:07:49 +0900

    Controller作成

--- src/main/java/com/example/weatherservice/WeatherController.java
+++ src/main/java/com/example/weatherservice/WeatherController.java
@@ -1,4 +1,13 @@
 package com.example.weatherservice;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WeatherController {
+
+    @RequestMapping("/weather")
+    public String getWeather() {
+        return "Sunny";
+    }
 }
