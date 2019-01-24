9df64c5cccf25e6941cdc48995af7721e0a387ea
otty375 <sie.jugend.15@gmail.com>
2019-01-24 21:13:01 +0900

    HTTPメソッド指定

--- src/main/java/com/example/weatherservice/WeatherController.java
+++ src/main/java/com/example/weatherservice/WeatherController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @RestController
 public class WeatherController {
 
-    @RequestMapping("/weather")
+    @RequestMapping(value = "/weather", method = GET)
     public String getWeather(@RequestParam(name = "city", defaultValue = "Tokyo") String city) {
         return city + ": Sunny";
     }
