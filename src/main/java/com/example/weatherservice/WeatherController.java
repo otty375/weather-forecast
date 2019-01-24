b2bc6601a145447d6f4f572840d18e9c6db11bbb
otty375 <sie.jugend.15@gmail.com>
2019-01-24 21:10:56 +0900

    リクエストパラメータ追加

--- src/main/java/com/example/weatherservice/WeatherController.java
+++ src/main/java/com/example/weatherservice/WeatherController.java
@@ -1,13 +1,14 @@
 package com.example.weatherservice;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WeatherController {
 
     @RequestMapping("/weather")
-    public String getWeather() {
-        return "Sunny";
+    public String getWeather(@RequestParam(name = "city", defaultValue = "Tokyo") String city) {
+        return city + ": Sunny";
     }
 }
