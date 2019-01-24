eac9dbf3ecf13b7ad426b85220bfa1a9e7673aca
otty375 <sie.jugend.15@gmail.com>
2019-01-24 20:48:12 +0900

    first commit

---
+++ src/main/java/com/example/weatherservice/WeatherServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.weatherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherServiceApplication.class, args);
+	}
+
+}
+
