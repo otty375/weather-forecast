ad5a060074ee5ba6da750522439fa4819244bf3c
otty375 <sie.jugend.15@gmail.com>
2019-01-30 20:42:13 +0900

    thymeleaf導入

--- src/main/java/com/example/weatherforecast/WeatherController.java
+++ src/main/java/com/example/weatherforecast/WeatherController.java
@@ -1,22 +1,23 @@
 package com.example.weatherforecast;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@RestController
+@Controller
 public class WeatherController {
 
     @Autowired
     WeatherForecastService weatherService;
 
     @RequestMapping(value = "/weather", method = GET)
-    public String getWeather(@RequestParam(name = "city", defaultValue = "Tokyo") String city) {
+    public String getWeather(Model model, @RequestParam(name = "city", defaultValue = "Tokyo") String city) {
         WeatherForecastDto dto = weatherService.getWeather(city);
-
-        return dto.getTodayForecast().getTelop();
+        model.addAttribute("message", dto.getTodayForecast().getTelop());
+        return "weather";
     }
 }
