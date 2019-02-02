49e81ffdf3422d71773ea6bda08d265ab675ae3e
otty375 <sie.jugend.15@gmail.com>
2019-02-02 11:47:13 +0900

    restTemplateをbean化

--- src/main/java/com/example/weatherforecast/service/WeatherForecastServiceImpl.java
+++ src/main/java/com/example/weatherforecast/service/WeatherForecastServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.weatherforecast.service;
 
 import com.example.weatherforecast.CityEnum;
 import com.example.weatherforecast.resource.WeatherForecast;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,11 +11,14 @@ public class WeatherForecastServiceImpl implements WeatherForecastService {
 
     private final String WEATHER_URL = "http://weather.livedoor.com/forecast/webservice/json/v1?city=";
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Override
     public WeatherForecast getWeather(String city) {
+        // リクエストパラメータからIdを取得
         String cityId = CityEnum.valueOf(city.toUpperCase()).getId();
-
-        RestTemplate restTemplate = new RestTemplate();
+        // 天気予報APIを叩く
         WeatherForecast response = restTemplate.getForObject(WEATHER_URL + cityId, WeatherForecast.class);
 
         return response;
