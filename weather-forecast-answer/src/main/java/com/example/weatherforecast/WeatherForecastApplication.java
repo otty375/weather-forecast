82490a92e095f30d07173efea5580f6445cfac40
otty375 <sie.jugend.15@gmail.com>
2019-02-19 22:51:30 +0900

    インデント修正

--- weather-forecast-answer/src/main/java/com/example/weatherforecast/WeatherForecastApplication.java
+++ weather-forecast-answer/src/main/java/com/example/weatherforecast/WeatherForecastApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherForecastApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherForecastApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherForecastApplication.class, args);
+    }
 
 }
 
