54c2bc2467eb7d6befb967d50a962c7aff2ec863
otty375 <sie.jugend.15@gmail.com>
2019-02-24 16:31:22 +0900

    クラス名の変更

--- weather-forecast-answer/src/main/java/com/example/weatherforecast/controller/WeatherForecastController.java
+++ weather-forecast-answer/src/main/java/com/example/weatherforecast/controller/WeatherForecastController.java
@@ -12,32 +12,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-// 演習3-1 コントローラのBean定義をしよう
+@Controller
+public class WeatherForecastController {
 
-public class WeatherController {
+    private final WeatherForecastService weatherForecastService;
 
-    // 演習6-1 WeatherServiceをコンストラクタインジェクションしよう
+    @Autowired
+    public WeatherForecastController(WeatherForecastService weatherForecastService) {
+        this.weatherForecastService = weatherForecastService;
+    }
 
-
-
-
-
-
-    // 演習3-2 リクエストマッピングの設定をしよう
-
-    public String getWeather(Model model, /** 演習3-3 リクエストパラメータの設定をしよう **/ String city) throws Exception {
+    @RequestMapping(value = "/weather", method = GET)
+    public String getWeather(Model model, @RequestParam(name = "city", defaultValue = "tokyo") String city) throws Exception {
         WeatherForecast weatherForecast = weatherForecastService.getWeather(city);
-
-        // 演習7-1 コントローラ層でModelに情報を詰めていることを確認しよう（編集不要）
         model.addAttribute("title", weatherForecast.getTitle());
         model.addAttribute("forecasts", weatherForecast.getForecasts());
-
-        // 演習7-2 コントローラ層でビュー名を返していることを確認しよう（編集不要）
         return "weather";
     }
 
-    // 演習8-2 コントローラ層で例外をハンドリングしてみよう
-
+    @ExceptionHandler({CityNotFoundException.class})
     public String handleError() {
         return "error";
     }
--- weather-forecast-handson/src/main/java/com/example/weatherforecast/controller/WeatherForecastController.java
+++ weather-forecast-handson/src/main/java/com/example/weatherforecast/controller/WeatherForecastController.java
@@ -12,25 +12,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@Controller
-public class WeatherController {
+// 演習3-1 コントローラのBean定義をしよう
 
-    private final WeatherForecastService weatherForecastService;
+public class WeatherForecastController {
 
-    @Autowired
-    public WeatherController(WeatherForecastService weatherService) {
-        this.weatherForecastService = weatherService;
-    }
+    // 演習6-1 WeatherServiceをコンストラクタインジェクションしよう
 
-    @RequestMapping(value = "/weather", method = GET)
-    public String getWeather(Model model, @RequestParam(name = "city", defaultValue = "tokyo") String city) throws Exception {
+
+
+
+
+
+    // 演習3-2 リクエストマッピングの設定をしよう
+
+    public String getWeather(Model model, /** 演習3-3 リクエストパラメータの設定をしよう **/ String city) throws Exception {
         WeatherForecast weatherForecast = weatherForecastService.getWeather(city);
+
+        // 演習7-1 コントローラ層でModelに情報を詰めていることを確認しよう（編集不要）
         model.addAttribute("title", weatherForecast.getTitle());
         model.addAttribute("forecasts", weatherForecast.getForecasts());
+
+        // 演習7-2 コントローラ層でビュー名を返していることを確認しよう（編集不要）
         return "weather";
     }
 
-    @ExceptionHandler({CityNotFoundException.class})
+    // 演習8-2 コントローラ層で例外をハンドリングしてみよう
+
     public String handleError() {
         return "error";
     }
